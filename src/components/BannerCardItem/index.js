c509cc874be7d246eb952001b18b3b4b387746aa
charishma <charishma.m.7@gmail.com>
2023-01-23 18:36:27 +0000

    first commit

---
+++ src/components/BannerCardItem/index.js
@@ -0,0 +1,20 @@
+// Write your code here.
+import './index.css'
+
+const BannerCardItem = props => {
+  const {bannerDetails} = props
+  const {headerText, description, className} = bannerDetails
+  return (
+    <li className={`card ${className}`}>
+      <div>
+        <h1 className="banner-heading">{headerText}</h1>
+        <p className="banner-text">{description}</p>
+        <button className="button" type="button">
+          Show More
+        </button>
+      </div>
+    </li>
+  )
+}
+
+export default BannerCardItem
